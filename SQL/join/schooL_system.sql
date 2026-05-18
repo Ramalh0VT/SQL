2deb4b18c2240c70e05c82b5c4e73a90396fc7fa
Ramalh0VT <gustavoramalhodasilva777@gmail.com>
2026-05-18 15:01:42 -0300

    Started learning about joins

---
+++ SQL/join/schooL_system.sql
@@ -0,0 +1,17 @@
+CREATE TABLE IF NOT EXISTS students(
+student_id INT auto_increment PRIMARY KEY,
+student_name VARCHAR(100) NOT NULL,
+birth_date DATE NULL
+)AUTO_INCREMENT = 1337;
+
+CREATE TABLE IF NOT EXISTS contacts(
+contact_id INT AUTO_INCREMENT,
+email VARCHAR(100) NOT NULL UNIQUE,
+phone VARCHAR(100) NOT NULL UNIQUE,
+student_id INT NOT NULL,
+CONSTRAINT FK_student
+FOREIGN KEY(student_id)
+REFERENCES students(student_id),
+PRIMARY KEY(contact_id)
+)AUTO_INCREMENT = 123;
+
